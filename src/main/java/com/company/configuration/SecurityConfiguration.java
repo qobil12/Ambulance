535dbd69450bc1dfe9895d4a33398baea75fab1c
Qobiljon <adilovadilov00@gmail.com>
2023-09-21 17:58:20 +0500

    Added something

--- src/main/java/com/company/configuration/SecurityConfiguration.java
+++ src/main/java/com/company/configuration/SecurityConfiguration.java
@@ -1,13 +1,13 @@
 //package com.company.configuration;
 //
 //import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.SecurityBuilder;
 //import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 //
 //@EnableWebSecurity
 //@Configuration
-//public class SecurityConfiguration extends Web {
+//public class SecurityConfiguration implements WebSecurityConfigurer {
 //
 //
 //}
